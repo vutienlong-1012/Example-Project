1591202021597145130f86cd3686b3eb184ac183
Long Vũ Tiến <vutienlong.1012@outlook.com.vn>
2023-12-31 00:10:33 +0700

    End of the year

---
+++ Assets/_ExampleProject/2_UI/LoadingScenePopup/LoadingScenePopup.cs
@@ -0,0 +1,22 @@
+using ExampleProject.UI;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ExampleProject.UI.LoadingScenePopup
+{
+    public class LoadingScenePopup : PopupBase
+    {
+        [SerializeField] Slider loadingSlider;
+        [SerializeField] Text progressText;
+
+        [Button]
+        public void SetProgress(float _value)
+        {
+            loadingSlider.value = _value;
+            //progressText.text = (Mathf.Clamp01(_value) * 100).ToString("N0") + "%";
+        }
+    }
+}
---
+++ Assets/_ExampleProject/Scenes/Loading Scene/FakeLoadMainScene.cs
@@ -0,0 +1,66 @@
+using ExampleProject.UI.LoadingScenePopup;
+using ExampleProject;
+using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using VTLTools;
+
+public class FakeLoadMainScene : Singleton<FakeLoadMainScene>
+{
+    [SerializeField] float fakeLoadTime;
+    [SerializeField] LoadingScenePopup loadingScenePopup;
+    //[SerializeField] bool isShowedFirstAOA = false;
+    public void StartFakeLoad()
+    {
+        Application.targetFrameRate = 60;
+#if UNITY_EDITOR
+        fakeLoadTime = 8;
+#endif
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    bool isLoadingGamplayScene;
+    bool isAlreadyCalledAOA;
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation _operation = null;
+        float _currentFakeLoadTime = fakeLoadTime;
+        float _progess = 0;
+        while (_progess < 1)
+        {
+            _currentFakeLoadTime -= Time.deltaTime;
+            _progess = 1 - (_currentFakeLoadTime / fakeLoadTime);
+            loadingScenePopup.SetProgress(_progess);
+
+            if (_progess >= 0.55 && !isAlreadyCalledAOA)
+            {
+                isAlreadyCalledAOA = true;
+                //if (CC_Interface.current.isJustLaunch)
+                //{
+                //    CC_Interface.current.isJustLaunch = false;
+                //    CC_Interface.current.ShowAppOpenAd();
+
+                //}
+            }
+
+            if (_progess >= 0.3 && !isLoadingGamplayScene)
+            {
+                _operation = SceneManager.LoadSceneAsync(1);
+                _operation.allowSceneActivation = false;
+                isLoadingGamplayScene = true;
+            }
+
+            yield return null;
+        }
+
+        _operation.allowSceneActivation = true;
+        _operation.completed += (AsyncOperation op) =>
+        {
+            //GameManager.instance.State = GameState.MainScene;
+            loadingScenePopup.Hide();
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("MainHomeScene")); ;
+        };
+    }
+}
